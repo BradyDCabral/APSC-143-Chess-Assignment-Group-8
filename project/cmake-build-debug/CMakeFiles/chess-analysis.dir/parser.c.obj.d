1061051dbc51473d9ca5a8d103a4bea6b26d971e
Owenchenyc <owenchenyc@gmail.com>
2025-11-29 17:19:04 -0500

    OWEN

---
+++ project/cmake-build-debug/CMakeFiles/chess-analysis.dir/parser.c.obj.d
@@ -0,0 +1,17 @@
+CMakeFiles/chess-analysis.dir/parser.c.obj: \
+ C:\Users\owenc\OneDrive\Documents\GitHub\APSC-143-Chess-Assignment-Group-8\project\parser.c \
+ C:\Users\owenc\OneDrive\Documents\GitHub\APSC-143-Chess-Assignment-Group-8\project\parser.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdbool.h \
+ C:\Users\owenc\OneDrive\Documents\GitHub\APSC-143-Chess-Assignment-Group-8\project\board.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/owenc/Downloads/mingw64/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:\Users\owenc\OneDrive\Documents\GitHub\APSC-143-Chess-Assignment-Group-8\project\panic.h
